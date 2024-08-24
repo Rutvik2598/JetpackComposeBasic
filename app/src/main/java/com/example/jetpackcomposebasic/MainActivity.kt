c59da761feacd0fcbcf7f597fa2599b005927819
Rutvik <kulkarnirutvikvishwas@gmail.com>
2024-08-24 13:14:01 -0400

    Adding basic Texts, Image and Icon

--- app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposebasic.ui.theme.JetpackComposeBasicTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +45,39 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Gray)
+    ) {
+        Text(
+            text = "Hello $name!",
+            color = Color.Red,
+            fontSize = 30.sp,
+            modifier = Modifier
+                .background(Color.LightGray)
+                .padding(16.dp)
+        )
+
+        Text(
+            text = "Second text",
+            color = Color.Blue,
+            fontSize = 30.sp,
+            modifier = Modifier
+                .background(Color.Yellow)
+                .padding(16.dp)
+        )
+
+        Image(painter = painterResource(
+            id = R.drawable.ic_launcher_foreground),
+            contentDescription = "Android Icon",
+            modifier = Modifier.background(Color.White)
+        )
+
+        Icon(imageVector = Icons.Default.Face, contentDescription = "Face icon")
+    }
 }
 
 @Preview(showBackground = true)
