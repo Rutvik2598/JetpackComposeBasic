85a072e2d419b4020ae942d61932723f74dc67b6
Rutvik <kulkarnirutvikvishwas@gmail.com>
2024-08-26 22:20:23 -0400

    Counter example

--- app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
@@ -15,10 +15,15 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Face
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -35,77 +40,62 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetpackComposeBasicTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ListView(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                var count by remember {
+                    mutableStateOf(0)
+                }
+                Column (
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                ) {
+                    Text(
+                        text = count.toString(),
+                        fontSize = 30.sp
                     )
+                    Button(onClick = {
+                        count++
+                    }) {
+                        Text(text = "Click me")
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column (
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Gray)
-    ) {
-        Text(
-            text = "Hello $name!",
-            color = Color.Red,
-            fontSize = 30.sp,
-            modifier = Modifier
-                .background(Color.LightGray)
-                .padding(16.dp)
-        )
-
-        Text(
-            text = "Second text",
-            color = Color.Blue,
-            fontSize = 30.sp,
-            modifier = Modifier
-                .background(Color.Yellow)
-                .padding(16.dp)
-        )
-
-        Image(painter = painterResource(
-            id = R.drawable.ic_launcher_foreground),
-            contentDescription = "Android Icon",
-            modifier = Modifier.background(Color.White)
-        )
-
-        Icon(imageVector = Icons.Default.Face, contentDescription = "Face icon")
-    }
-}
-
-//@Preview(showBackground = true)
 //@Composable
-//fun GreetingPreview() {
-//    JetpackComposeBasicTheme {
-//        Greeting("Android")
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Column (
+//        horizontalAlignment = Alignment.CenterHorizontally,
+//        verticalArrangement = Arrangement.Center,
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .background(Color.Gray)
+//    ) {
+//        Text(
+//            text = "Hello $name!",
+//            color = Color.Red,
+//            fontSize = 30.sp,
+//            modifier = Modifier
+//                .background(Color.LightGray)
+//                .padding(16.dp)
+//        )
+//
+//        Text(
+//            text = "Second text",
+//            color = Color.Blue,
+//            fontSize = 30.sp,
+//            modifier = Modifier
+//                .background(Color.Yellow)
+//                .padding(16.dp)
+//        )
+//
+//        Image(painter = painterResource(
+//            id = R.drawable.ic_launcher_foreground),
+//            contentDescription = "Android Icon",
+//            modifier = Modifier.background(Color.White)
+//        )
+//
+//        Icon(imageVector = Icons.Default.Face, contentDescription = "Face icon")
 //    }
 //}
-
-@Composable
-fun ListView(name: String, modifier: Modifier = Modifier) {
-    LazyColumn (
-        modifier = Modifier.fillMaxSize()
-    ) {
-        items(10) { i ->
-            Icon(imageVector = Icons.Default.Add, contentDescription = "List view", modifier = Modifier.size(100.dp))
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun ListPreview() {
-    JetpackComposeBasicTheme {
-        ListView("Android")
-    }
-}
