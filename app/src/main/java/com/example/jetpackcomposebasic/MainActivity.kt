1e2c234495ac496ac292fa299cd89648df3189ed
Rutvik <kulkarnirutvikvishwas@gmail.com>
2024-08-26 23:04:55 -0400

    Populating lazy list with using a text field

--- app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.jetpackcomposebasic
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -8,15 +9,24 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -33,6 +43,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposebasic.ui.theme.JetpackComposeBasicTheme
+import org.w3c.dom.NameList
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,25 +51,56 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetpackComposeBasicTheme {
-                var count by remember {
-                    mutableStateOf(0)
+                var name by remember {
+                    mutableStateOf("")
                 }
+                var names by remember {
+                    mutableStateOf(listOf<String>())
+                }
+                Spacer(modifier = Modifier.height(100.dp))
                 Column (
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
                     horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
                 ) {
-                    Text(
-                        text = count.toString(),
-                        fontSize = 30.sp
-                    )
-                    Button(onClick = {
-                        count++
-                    }) {
-                        Text(text = "Click me")
+                    Row (
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        OutlinedTextField(
+                            value = name,
+                            onValueChange = { text ->
+                                name = text
+                        } )
+                        
+                        Spacer(modifier = Modifier.width(16.dp))
+                        
+                        Button(onClick = {
+                            if (name.isNotBlank())
+                                names = names + name
+                                name = ""
+                        }) {
+                            Text(text = "Add name")
+                        }
                     }
+                    NameList(names = names)
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun NameList(names: List<String>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier) {
+        items(names) { currentName ->
+            Text(
+                text = currentName,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+            HorizontalDivider()
         }
     }
 }
